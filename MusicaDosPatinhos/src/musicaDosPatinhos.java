c3b1f53658e0960f3033de02795b2c7224ab408e
Jaqueline Cantarino <jaquelinecantarino@outlook.com>
2022-05-07 23:30:45 -0300

    musica dos patinhos

--- MusicaDosPatinhos/src/musicaDosPatinhos.java
+++ MusicaDosPatinhos/src/musicaDosPatinhos.java
@@ -7,7 +7,7 @@ public class musicaDosPatinhos {
 		
 		int patinhos=0;
 		
-		System.out.println("Quantos patinhos v„o ter na musica?");
+		System.out.println("Quantos patinhos v√£o ter na musica?");
 		patinhos=leia.nextInt();
 		
 		for(int n=0; n<=patinhos; n++) {
@@ -18,32 +18,32 @@ public class musicaDosPatinhos {
 				
 				n++;
 				n++;
-				//pontos para aprimorar: Por que eu preciso somar 2 no N para n„o duplicar
+				//pontos para aprimorar: Por que eu preciso somar 2 no N para n√£o duplicar
 				//
 				
-				System.out.printf("\n" + x + " patinhos foram passear\r\n"
-						+ "AlÈm das montanhas\r\n"
+				System.out.printf("\nü¶Ü " + x + " patinhos foram passear\r\n"
+						+ "Al√©m das montanhas\r\n"
 						+ "Para brincar\r\n"
-						+ "A mam„e gritou: Qu·, qu·, qu·, qu·\r\n"
-						+ "Mas sÛ " + (x-1) +" patinhos voltaram de l·.\n");
+						+ "A mam√£e gritou: Qu√°, qu√°, qu√°, qu√°\r\n"
+						+ "Mas s√≥ " + (x-1) +" patinhos voltaram de l√°.\n");
 				
 				x=x-1; //x--;
 				
 			}while(x!=1);
 			
-			System.out.printf("\n1 patinhos foram passear\r\n"
-					+ "AlÈm das montanhas\r\n"
+			System.out.printf("\n\nü¶Ü 1 patinho foi passear\r\n"
+					+ "Al√©m das montanhas\r\n"
 					+ "Para brincar\r\n"
-					+ "A mam„e gritou: Qu·, qu·, qu·, qu·\r\n"
-					+ "Mas nenhum patinho voltou de l·.\n");
+					+ "A mam√£e gritou: Qu√°, qu√°, qu√°, qu√°\r\n"
+					+ "Mas nenhum patinho voltou de l√°.\n");
 				
 		}
 		
-		System.out.printf("\nA mam„e ficou t„o triste! \nA mam„e patinha foi procurar\r\n"
-				+ "AlÈm das montanhas\r\n"
+		System.out.printf("\nA mam√£e ficou t√£o triste! \nA mam√£e patinha foi procurar\r\n"
+				+ "Al√©m das montanhas\r\n"
 				+ "Na beira do mar\r\n"
-				+ "A mam„e gritou: Qu·, qu·, qu·, qu·\r\n"
-				+ "E os %d patinhos voltaram de l·.", + patinhos);
+				+ "A mam√£e gritou: Qu√°, qu√°, qu√°, qu√°\r\n"
+				+ "E os %d patinhos voltaram de l√°. ü¶Üü¶Üü¶Üü¶Üü¶Ü", + patinhos);
 
 	}
 
